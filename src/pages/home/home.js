72e75b7a97c95b84722387c189ee6bcf26910dba
meetdobariya01 <meetdobariya87@gmail.com>
2025-12-04 14:55:58 +0530

    add files

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+      home
+      <Footer />
+    </div>
+  )
+}
+
+export default Home;
